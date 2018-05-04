20d06a98235e266acf15c9dc9e9771ae3500531f
d4rk413x21 <darkalex21@gmail.com>
2018-05-04 17:11:53 +0200

    commit iniziale

---
+++ config.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Manjesh on 14-05-2016.
+ */
+
+module.exports = {
+  sql: {
+    database: 'oauth_demo',
+    username: 'root',
+    password: '',
+    dialect: 'mysql', // PostgreSQL, MySQL, MariaDB, SQLite and MSSQL See more: http://docs.sequelizejs.com/en/latest/
+    logging: true,
+    timezone: '+05:30',
+  },
+  mongo: {
+    uri: 'mongodb://localhost:27017/SSO'
+  },
+  seedDB:false,
+  seedMongoDB:false,
+  seedDBForce:true,
+  db:'mongo' // mongo,sql if you want to use any SQL change dialect above in sql config
+}
